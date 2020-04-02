3527da2294e10d7c13ae0f468e95565b1fff389d
Angela <angela.inniss@justpark.com>
2020-04-02 15:44:08 +0100

    add checkout summary

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./conainers/BurgerBuilder/BurgerBuilder";
 
+import Checkout from "./conainers/Checkout/checkout";
+
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
