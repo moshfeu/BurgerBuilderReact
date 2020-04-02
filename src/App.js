75171e2bc0a4111ae47c131968ad63e07b3f7c61
Angela <angela.inniss@justpark.com>
2020-04-02 16:27:33 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./conainers/BurgerBuilder/BurgerBuilder";
-
 import Checkout from "./conainers/Checkout/checkout";
 
 class App extends Component {
@@ -9,8 +10,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
@@ -20,3 +23,5 @@ class App extends Component {
 export default App;
 // Layout is a component contains the Nav bar and takes children which will be the burger app content.
 // we have wrapped the Layout component around our content.
+// "exact the order doesn't matter as it is not treated as a prefix
+// Switch makes app load the first matched path
