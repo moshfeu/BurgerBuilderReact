4dfd07b767fd02b406492948a580f379e3e8d986
Angela <angela.inniss@justpark.com>
2020-04-03 19:18:26 +0100

    Add comments

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -8,6 +8,8 @@ const Order = props => {
       name: ingredientName,
       amount: props.ingredients[ingredientName]
     }); // value of ingredients
+    // console.log(ingredientName); name of ingredient Bacon, Salad etc.
+    // console.log(props.ingredients[ingredientName]) amount of each ingredient 1,2,3
   }
 
   const ingredientOutput = ingredients.map(ig => {
@@ -36,3 +38,8 @@ const Order = props => {
 };
 
 export default Order;
+
+// we loop through the ingredients array and create an object with the ingredient name and amount and store in ingredients array 6 - 10
+// then we map over the newly created ingredients array  (store in a variable) and return the name and amount with styles line 25 -17
+// then we return the ingredients and price -lines 34- 25
+//not sure how we have access to ingredients in this file or price - need to find out.
--- src/conainers/Orders/Orders.js
+++ src/conainers/Orders/Orders.js
@@ -5,13 +5,16 @@ import axios from "../../axios-orders";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component {
-  state = { orders: [], loading: true };
+  state = {
+    orders: [],
+    loading: true
+  };
   // using component did mount because we only want to fetch orders once this is mounted
   componentDidMount() {
     axios
       .get("/orders.json")
       .then(res => {
-        // console.log(res.data);
+         console.log(res.data);
         const fetchedOrders = [];
         for (let key in res.data) {
           fetchedOrders.push({ ...res.data[key], id: key });
@@ -37,3 +40,9 @@ class Orders extends Component {
   }
 }
 export default withErrorHandler(Orders, axios);
+
+// this file is pulling the orders from the DB
+// then looping through each order and storing the order key in an array called "FetchedOrders"
+// the orders state is the updated with fetchedOrders (See state).
+// in the return we then map over all the orders from the state (which is a list of keys)
+// this then gets passed to the Order component
