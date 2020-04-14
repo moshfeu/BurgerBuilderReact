32b7d4e09ba5095d102f42314acaca6288cd951b
Angela <angela.inniss@justpark.com>
2020-04-14 14:06:21 +0100

    Aadd thunk middleware

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {createStore} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 
-const store = createStore(burgerBuilderReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+const store = createStore(
+  burgerBuilderReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>
@@ -23,3 +30,6 @@ ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
 
 // Provider allows us to add the reducer store that we created so we can manage state
+// composeEnhancers allows us to use asyncronous code in our action creators.
+// applyMiddleware needs to be passed into the composeEnhancers with the thunk argument which is the middleware i want to apply
+// apply middleware is one kind of enhancer, dev tools is another. "Compose" allows us to apply this middleware
