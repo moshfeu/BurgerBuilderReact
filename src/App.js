ab2fd9604c212bc1b9aff973e618679fa9e5fbcc
Angela <angela.inniss@justpark.com>
2020-04-20 14:07:32 +0100

    only show logged in users orders on order page using userId param in order request

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
           <Route path="/logout" exact component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
       );
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -10,7 +10,7 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 class Orders extends Component {
   // using component did mount because we only want to fetch orders once this is mounted
   componentDidMount() {
-    this.props.onFetchOrders(this.props.token);
+    this.props.onFetchOrders(this.props.token, this.props.userId);
   }
   render() {
     let orders = <Spinner />;
@@ -31,13 +31,14 @@ const mapStateToProps = state => {
   return {
     orders: state.order.orders,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   };
 };
 const mapDispatchToProps = dispatch => {
   return {
-    onFetchOrders: (token) => {
-      dispatch(actions.fetchOrders(token));
+    onFetchOrders: (token, userId) => {
+      dispatch(actions.fetchOrders(token, userId));
     }
   };
 };
