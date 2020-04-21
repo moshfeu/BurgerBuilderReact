11696876da4bc37f8bd3126bf38abc79ac2f7fdf
Angela <angela.inniss@justpark.com>
2020-04-21 17:22:10 +0100

    try to fix toggle transition

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import Backdrop from "../../UI/Backdrop/Backdrop";
@@ -6,32 +6,35 @@ import Aux from "../../../hoc/Aux";
 
 import classes from "./SideDrawer.css";
 
-const SideDrawer = props => {
-  return (
-    <Aux classname={classes.SideDrawer}>
-      <Backdrop
-        showBackdrop={props.showSideDrawer}
-        clicked={props.toggleSideDrawer}
-      />
-      {props.showSideDrawer && (
-        <div
-          onClick={props.toggleSideDrawer}
-          className={
-            props.toggleSideDrawer ? classes.SideDrawerOpen : classes.SideDrawer
-          }
-        >
-          <div className={classes.Logo}>
-            <Logo />
-          </div>
+class SideDrawer extends Component {
+  render() {
+    let sideDrawerClass = [classes.SideDrawer];
+    // SideDrawer will now be an array  with the side drawer classes and the open class
+    sideDrawerClass.push(classes.Open);
+    return (
+      <Aux classname={classes.SideDrawer}>
+        <Backdrop
+          showBackdrop={this.props.showSideDrawer}
+          clicked={this.props.toggleSideDrawer}
+        />
+        {this.props.showSideDrawer && (
+          <div
+            className={sideDrawerClass.join(" ")}
+            onClick={this.props.toggleSideDrawer}
+          >
+            <div className={classes.Logo}>
+              <Logo />
+            </div>
 
-          <nav>
-            <NavigationItems />
-          </nav>
-        </div>
-      )}
-    </Aux>
-  );
-};
+            <nav>
+              <NavigationItems />
+            </nav>
+          </div>
+        )}
+      </Aux>
+    );
+  }
+}
 
 export default SideDrawer;
 
